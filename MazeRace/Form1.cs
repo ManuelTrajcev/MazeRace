35ba245bcebbeeb70498637a3a5ce7fce97b003f
ManuelTrajcev <manuel.trajchev@students.finki.ukim.mk>
2024-07-04 18:17:15 +0200

    Game class

--- MazeRace/Form1.cs
+++ MazeRace/Form1.cs
@@ -10,64 +10,51 @@ using System.Windows.Forms;
 namespace MazeRace
 {
     public partial class Form1 : Form
-    {
-        private Player Player;
-        private Player PC;
-        private Point Finish;
-        private int Level;
-        private int PCSpeed;
-        List<Point> coins = new List<Point>();
-        private int[,] maze;
+    { 
         private Timer gameTimer;
         private bool isDisabled = false;
         private static MazeGenerator MazeGenerator = new MazeGenerator();
+        private Game Game;
         public Form1()
         {
             InitializeComponent();
             this.DoubleBuffered = true;
             this.Height = 600;
             this.Width = 600;
-            PCSpeed = 1050;
-            Player = new Player(new Point(1, 1));
-            PC = new Player(new Point(1, 1));
-            Level = 0;
-            ssScore.Text = $"Player: {Player.Score}    Computer: {PC.Score}";
+            Game = new Game();
+            ssScore.Text = $"Player: {Game.Player.Score}    Computer: {Game.PC.Score}";
             gameTimer = new Timer();
             StartNextLevel();
         }
 
         private void gameTimer_Tick(object sender, EventArgs e)
         {
-            List<Point> pathToFinish = MazeSolver.FindShortestPath(maze, PC.Position, Finish);
-
+            List<Point> pathToFinish = MazeSolver.FindShortestPath(Game.maze, Game.PC.Position, Game.Finish);
             List<Point> path = pathToFinish;
-            if (coins.Count > 0)
+            if (Game.coins.Count > 0)
             {
-                List<Point> pathToClosestCoin = MazeSolver.FindShortestPath(maze, PC.Position, coins[0]);
-                coins.ForEach(c =>
+                List<Point> pathToClosestCoin = MazeSolver.FindShortestPath(Game.maze, Game.PC.Position, Game.coins[0]);
+                Game.coins.ForEach(c =>
                 {
-                    if (MazeSolver.FindShortestPath(maze, PC.Position, c).Count < pathToClosestCoin.Count)
+                    if (MazeSolver.FindShortestPath(Game.maze, Game.PC.Position, c).Count < pathToClosestCoin.Count)
                     {
-                        pathToClosestCoin = MazeSolver.FindShortestPath(maze, PC.Position, c);
+                        pathToClosestCoin = MazeSolver.FindShortestPath(Game.maze, Game.PC.Position, c);
                     }
                 });
                 path = pathToFinish.Count > pathToClosestCoin.Count && pathToClosestCoin.Count > 1 ? pathToClosestCoin : pathToFinish;
-
             }
-
-
 
             if (path != null && path.Count > 1)
             {
                 Point bestMove = path[1];
-                checkMovement(bestMove, true);
+                Game.checkMovement(bestMove, true);
             }
             Invalidate();
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            Point newPosition = Player.Position;
+            Point newPosition = Game.Player.Position;
             switch (e.KeyCode)
             {
                 case Keys.W:
@@ -84,100 +71,27 @@ namespace MazeRace
                     break;
             }
 
-            checkMovement(newPosition, false);
-
+            Game.checkMovement(newPosition, false);
+            UpdateScores();
+            Invalidate();
         }
 
         private void checkMovement(Point newPosition, bool isPC)
         {
-            if (maze[newPosition.Y, newPosition.X] == 1 || isDisabled)
-            {
-                return;
-            }
-            if (isPC)
-            {
-                if (maze[newPosition.Y, newPosition.X] == 2)
-                {
-                    PC.UpdateScore(10);
-                    maze[newPosition.Y, newPosition.X] = 0;
-                    coins.Remove(newPosition);
-                }
-                else if (maze[newPosition.Y, newPosition.X] == 3)
-                {
-                    PC.UpdateScore(10);
-                    PC.Move(newPosition);
-                    Invalidate();
-                    isDisabled = true;
-                    DialogResult result = MessageBox.Show("PC reached the finish line!", "Finish Line", MessageBoxButtons.OK);
-                    if (result == DialogResult.OK)
-                    {
-                        StartNextLevel();
-                        return;
-                    }
-                    StartNextLevel();
-                }
-                PC.Move(newPosition);
+                Game.Player.Move(newPosition);
                 UpdateScores();
                 Invalidate();
-            }
-            else
-            {
-                if (maze[newPosition.Y, newPosition.X] == 2)
-                {
-                    Player.UpdateScore(10);
-                    maze[newPosition.Y, newPosition.X] = 0;
-                    coins.Remove(newPosition);
-                }
-                else if (maze[newPosition.Y, newPosition.X] == 3)
-                {
-
-                    Player.UpdateScore(10);
-                    Player.Move(newPosition);
-                    Invalidate();
-                    isDisabled = true;
-                    DialogResult result = MessageBox.Show("Player reached the finish line!", "Finish Line", MessageBoxButtons.OK);
-                    if (result == DialogResult.OK)
-                    {
-                        StartNextLevel();
-                        return;
-                    }
-
-                }
-                Player.Move(newPosition);
-                UpdateScores();
-                Invalidate();
-            }
         }
 
         private void StartNextLevel()
         {
-            Level++;
-            gameTimer.Stop();
-            PC.setPosition(new Point(1, 1));
-            Player.setPosition(new Point(1, 1));
-            coins = new List<Point>();
-            maze = MazeGenerator.GenerateMaze(Level);
-            for (int y = 0; y < maze.GetLength(0); y++)
-            {
-                for (int x = 0; x < maze.GetLength(1); x++)
-                {
-                    if (maze[y, x] == 2)
-                    {
-                        coins.Add(new Point(x, y));
-                    }
-                    else if (maze[y, x] == 3)
-                    {
-                        Finish = new Point(x, y);
-                    }
-                }
-            }
+            Game.StartNextLevel();
             gameTimer = new Timer();
             isDisabled = false;
-            PCSpeed -= 50;
-            gameTimer.Interval = PCSpeed;
+            Game.PCSpeed -= 50;
+            gameTimer.Interval = Game.PCSpeed;
             gameTimer.Tick += gameTimer_Tick;
-            gameTimer.Start();
-            
+            gameTimer.Start(); 
         }
 
         protected override void OnPaint(PaintEventArgs e)
@@ -186,19 +100,19 @@ namespace MazeRace
             Graphics g = e.Graphics;
 
             int cellSize = 20;
-            int mazeWidth = maze.GetLength(1) * cellSize;
-            int mazeHeight = maze.GetLength(0) * cellSize;
+            int mazeWidth = Game.maze.GetLength(1) * cellSize;
+            int mazeHeight = Game.maze.GetLength(0) * cellSize;
 
             // Calculate offsets to center the maze
             int offsetX = (ClientSize.Width - mazeWidth) / 2;
             int offsetY = (ClientSize.Height - mazeHeight) / 2;
 
-            for (int y = 0; y < maze.GetLength(0); y++)
+            for (int y = 0; y < Game.maze.GetLength(0); y++)
             {
-                for (int x = 0; x < maze.GetLength(1); x++)
+                for (int x = 0; x < Game.maze.GetLength(1); x++)
                 {
                     Brush brush;
-                    switch (maze[y, x])
+                    switch (Game.maze[y, x])
                     {
                         case 1:
                             brush = Brushes.Black;  // Wall
@@ -221,18 +135,18 @@ namespace MazeRace
             }
 
             // Draw players
-            int playerDrawX = Player.Position.X * cellSize + offsetX;
-            int playerDrawY = Player.Position.Y * cellSize + offsetY;
+            int playerDrawX = Game.Player.Position.X * cellSize + offsetX;
+            int playerDrawY = Game.Player.Position.Y * cellSize + offsetY;
             g.FillEllipse(Brushes.Blue, playerDrawX, playerDrawY, cellSize, cellSize);
 
-            int pcDrawX = PC.Position.X * cellSize + offsetX;
-            int pcDrawY = PC.Position.Y * cellSize + offsetY;
+            int pcDrawX = Game.PC.Position.X * cellSize + offsetX;
+            int pcDrawY = Game.PC.Position.Y * cellSize + offsetY;
             g.FillEllipse(Brushes.Red, pcDrawX, pcDrawY, cellSize, cellSize);
         }
 
         private void UpdateScores()
         {
-            ssScore.Text = $"Player: {Player.Score}    Computer: {PC.Score}";
+            ssScore.Text = $"Player: {Game.Player.Score}    Computer: {Game.PC.Score}";
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
---
+++ MazeRace/Game.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MazeRace
+{
+    public class Game
+    {
+        public Player Player;
+        public Player PC;
+        public Point Finish;
+        public int Level;
+        public int PCSpeed;
+        public List<Point> coins = new List<Point>();
+        public int[,] maze;
+        public bool isDisabled = false;
+        public static MazeGenerator MazeGenerator = new MazeGenerator();
+
+        public Game()
+        {
+            PCSpeed = 1050;
+            Player = new Player(new Point(1, 1));
+            PC = new Player(new Point(1, 1));
+            Level = 0;
+        }
+
+        public void checkMovement(Point newPosition, bool isPC)
+        {
+            if (maze[newPosition.Y, newPosition.X] == 1 || isDisabled)
+            {
+                return;
+            }
+            if (isPC)
+            {
+                if (maze[newPosition.Y, newPosition.X] == 2)
+                {
+                    PC.UpdateScore(10);
+                    maze[newPosition.Y, newPosition.X] = 0;
+                    coins.Remove(newPosition);
+                }
+                else if (maze[newPosition.Y, newPosition.X] == 3)
+                {
+                    PC.UpdateScore(10);
+                    PC.Move(newPosition);
+                    isDisabled = true;
+                    DialogResult result = MessageBox.Show("PC reached the finish line!", "Finish Line", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        StartNextLevel();
+                        return;
+                    }
+                    StartNextLevel();
+                }
+                PC.Move(newPosition);   
+            }
+            else
+            {
+                if (maze[newPosition.Y, newPosition.X] == 2)
+                {
+                    Player.UpdateScore(10);
+                    maze[newPosition.Y, newPosition.X] = 0;
+                    coins.Remove(newPosition);
+                }
+                else if (maze[newPosition.Y, newPosition.X] == 3)
+                {
+
+                   Player.UpdateScore(10);
+                    Player.Move(newPosition);
+                  
+                    isDisabled = true;
+                    DialogResult result = MessageBox.Show("Player reached the finish line!", "Finish Line", MessageBoxButtons.OK);
+                    if (result == DialogResult.OK)
+                    {
+                        StartNextLevel();
+                        return;
+                    }
+
+                }
+                Player.Move(newPosition);
+            
+            }
+        }
+
+        public void StartNextLevel()
+        {
+           Level++;
+            
+           PC.setPosition(new Point(1, 1));
+           Player.setPosition(new Point(1, 1));
+            coins = new List<Point>();
+           maze = MazeGenerator.GenerateMaze(Level);
+            for (int y = 0; y < maze.GetLength(0); y++)
+            {
+                for (int x = 0; x < maze.GetLength(1); x++)
+                {
+                    if (maze[y, x] == 2)
+                    {
+                        coins.Add(new Point(x, y));
+                    }
+                    else if (maze[y, x] == 3)
+                    {
+                        Finish = new Point(x, y);
+                    }
+                }
+            }
+            isDisabled = false;
+        }
+    }
+}
